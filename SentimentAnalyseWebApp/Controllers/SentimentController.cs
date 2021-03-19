fd01bb32f1bf371cdc6a784442e806d705c5867a
Daniel Basler <daniel.basler@web.de>
2021-03-19 09:49:36 +0100

    Add project files.

---
+++ SentimentAnalyseWebApp/Controllers/SentimentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using SentimentAnalyseWebAppML.Model;
+
+namespace SentimentAnalyseWebApp.Controllers
+{
+    public class SentimentController : Controller
+    {
+        [HttpGet]
+        public IActionResult SentimentAnalyse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SentimentAnalyse(ModelInput input)
+        {
+            
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load(@"..\SentimentAnalyseWebAppML.Model\MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            ModelOutput result = predEngine.Predict(input);
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
